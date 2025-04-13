b6a6498096031a9d77c616e16659bed3fc708485
Milza <marcomilza@gmail.com>
2025-04-13 13:10:29 +0200

    new export

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,29 @@
 import * as N from 'react-native';
 import { useHomeScreen } from './homeScreen.hook';
-import { styles } from './homeScreen.styles';
+import { S } from './homeScreen.styles';
 
 const HomeScreen = () => {
   const h = useHomeScreen();
   return (
-    <N.View style={styles.mainBox}>
-      <N.View style={styles.mainBox}>
+    <N.View style={S.mainBox}>
+      <N.View style={S.clearTextContainer}>
+        <N.Text style={S.clearText} onPress={() => h.setGoals([])}>
+          Clear
+        </N.Text>
+      </N.View>
+      <N.View style={S.mainBox}>
         <N.TextInput
-          style={styles.input}
+          style={S.input}
           placeholder="Your course goal"
           value={h.goal}
           onChangeText={h.setGoal}
         />
-        <N.Text style={styles.sameStringEr}>{h.sameStringEr}</N.Text>
+        <N.Text style={S.sameStringEr}>{h.sameStringEr}</N.Text>
         <N.Button title="Tap to add" onPress={() => h.addSetGoals(h.goal)} />
       </N.View>
-      <N.View style={styles.goalsBox}>
+      <N.View style={S.goalsBox}>
         {h.goals.map((g, i) => (
-          <N.Text key={i} style={styles.goalLi}>
+          <N.Text key={i} style={S.goalLi}>
             {g}
           </N.Text>
         ))}
