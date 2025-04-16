1efdff7b9edd46cf80bf8498914a1afe7ddd00b0
Milza <marcomilza@gmail.com>
2025-04-16 14:57:58 +0200

    separeted logic for homescreen

--- components/homescreen/homeScreen.hook.tsx
+++ components/homescreen/homeScreen.hook.tsx
@@ -1,26 +1,55 @@
-import { useState } from 'react';
-import { IAddSetGoals } from './homeScreen.models';
+import * as R from 'react';
+import { IAddSetGoals, IGoal } from './homeScreen.models';
 import { isSame } from './homeScreen.utils';
+import * as N from 'react-native';
 
 const useHomeScreen = () => {
   const sameStringWarn = 'You entered the same string! You moron!';
-  const [goals, setGoals] = useState<string[]>([]);
-  const [goal, setGoal] = useState('');
-  const [sameStringEr, setSameStringEr] = useState('');
+  const [goals, setGoals] = R.useState<IGoal[]>([]);
+  const [goal, setGoal] = R.useState('');
+  const [sameStringEr, setSameStringEr] = R.useState('');
+  const [keyboardVisible, setKeyboardVisible] = R.useState(false);
 
-  const addSetGoals: IAddSetGoals = enteredText => {
+  R.useEffect(() => {
+    const showSub = N.Keyboard.addListener('keyboardDidShow', () => {
+      setKeyboardVisible(true);
+    });
+    const hideSub = N.Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardVisible(false);
+    });
+
+    return () => {
+      showSub.remove();
+      hideSub.remove();
+    };
+  }, []);
+
+  const addSetGoals: IAddSetGoals = () => {
     setGoal('');
     setGoals(pv => {
-      if (isSame(pv, enteredText)) {
+      if (
+        isSame(
+          pv.map(obj => obj.text),
+          goal
+        )
+      ) {
         setSameStringEr(sameStringWarn);
         return pv;
       }
       sameStringEr === '' ? null : setSameStringEr('');
-      return [...pv, goal];
+      return [...pv, { text: goal, key: goal }];
     });
   };
 
-  return { goal, setGoal, goals, addSetGoals, sameStringEr, setGoals };
+  return {
+    goal,
+    setGoal,
+    goals,
+    addSetGoals,
+    sameStringEr,
+    setGoals,
+    keyboardVisible,
+  };
 };
 
 export { useHomeScreen };
