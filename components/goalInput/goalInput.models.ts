019574420b402ac51d992f817615fa3f40d5dff2
Milza <marcomilza@gmail.com>
2025-04-17 12:29:16 +0200

    removed unused

--- components/goalInput/goalInput.models.ts
+++
@@ -1,9 +0,0 @@
-interface IGoalInputProps {
-  goal: string;
-  sameStringEr: string;
-  addSetGoals: (goal: string) => void;
-  setGoal: React.Dispatch<React.SetStateAction<string>>;
-}
-type IGoalInput = React.FC<IGoalInputProps>;
-
-export { IGoalInput };
