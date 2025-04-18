9daf2db93d95b667332480bad08c821822738760
Milza <marcomilza@gmail.com>
2025-04-18 16:14:54 +0200

    goal item updated

--- components/homescreen/goalItem/goalItem.tsx
+++ components/homescreen/goalItem/goalItem.tsx
@@ -1,21 +1,21 @@
 import * as N from 'react-native';
 import * as GS from '../../../styles';
-import { S } from './goalItem.style';
+import { s, pressedGoalLi } from './goalItem.style';
 import { IGoalItem } from './goalItem.models';
 
 const GoalItem: IGoalItem = p => (
   <N.Pressable
-    style={S.goalLiBox}
+    style={p => (p.pressed ? pressedGoalLi : s.goalLiBox)}
     {...GS.android_ripple}
     onPress={() => p.check(p.goal)}
   >
-    <N.View style={S.textAndCheckBox}>
-      <N.Text style={S.checkText}>{p.goal.isChecked ? '✓' : '-'}</N.Text>
-      <N.Text style={S.goalLi}>{p.goal.text}</N.Text>
+    <N.View style={s.textAndCheckBox}>
+      <N.Text style={s.checkText}>{p.goal.isChecked ? '✓' : '-'}</N.Text>
+      <N.Text style={s.goalLi}>{p.goal.text}</N.Text>
     </N.View>
     <N.Pressable onPress={() => p.remove(p.goal)}>
-      <N.View style={S.xBox}>
-        <N.Text style={S.x}>X</N.Text>
+      <N.View style={s.xBox}>
+        <N.Text style={s.x}>X</N.Text>
       </N.View>
     </N.Pressable>
   </N.Pressable>
