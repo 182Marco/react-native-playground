e48a3e0dd9e49ccb8356c2d8150ebd63d5754aa5
Milza <marcomilza@gmail.com>
2025-04-20 14:26:21 +0200

    updated root

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,5 @@
-import * as N from 'react-native';
-import { useHomeScreen } from './homeScreen.hook';
-import { S } from './homeScreen.styles';
+import { HomeScreen } from '@/components';
 
-const HomeScreen = () => {
-  const h = useHomeScreen();
-  return (
-    <N.View style={S.mainBox}>
-      <N.View style={S.clearTextContainer}>
-        <N.Text style={S.clearText} onPress={() => h.setGoals([])}>
-          Clear
-        </N.Text>
-      </N.View>
-      <N.View style={S.mainBox}>
-        <N.TextInput
-          style={S.input}
-          placeholder="Your course goal"
-          value={h.goal}
-          onChangeText={h.setGoal}
-        />
-        <N.Text style={S.sameStringEr}>{h.sameStringEr}</N.Text>
-        <N.Button title="Tap to add" onPress={() => h.addSetGoals(h.goal)} />
-      </N.View>
-      <N.View style={S.goalsBox}>
-        {h.goals.map((g, i) => (
-          <N.Text key={i} style={S.goalLi}>
-            {g}
-          </N.Text>
-        ))}
-      </N.View>
-    </N.View>
-  );
-};
+const Root = () => <HomeScreen />;
 
-export default HomeScreen;
+export default Root;
