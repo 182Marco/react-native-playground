3b33a25227006ed99fde890bf8167c3d912e0714
Milza <marcomilza@gmail.com>
2025-04-21 19:16:48 +0200

    updated homescreen style

--- components/homescreen/homescreen.style.ts
+++ components/homescreen/homescreen.style.ts
@@ -1,70 +1,42 @@
-import * as N from 'react-native';
+import { StyleSheet } from 'react-native';
 import * as GolbalS from '../../styles';
 
-const S = N.StyleSheet.create({
-  mainBox: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: GolbalS.colors.background,
-    alignItems: 'center',
+const S = StyleSheet.create({
+  btnBox: {
+    flexDirection: 'row',
     justifyContent: 'center',
   },
-  clearTextContainer: {
-    backgroundColor: GolbalS.colors.errorColor,
-    paddingVertical: 8,
-    paddingHorizontal: 24,
-    borderRadius: 10,
-    marginTop: 40,
-    marginBottom: 16,
-  },
-  clearText: {
-    color: GolbalS.colors.textColor,
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  goalsBox: {
-    marginTop: 30,
-    width: '100%',
+  mainBox: {
+    flex: 1,
+    backgroundColor: GolbalS.colors.background,
     padding: 16,
+  },
+  clearTextBox: {
+    marginTop: 10,
+    flex: 1,
     backgroundColor: GolbalS.colors.panel,
     borderRadius: 12,
-    maxHeight: 250,
-  },
-  input: {
-    width: 310,
     padding: 16,
-    fontSize: 18,
-    borderRadius: 10,
-    backgroundColor: GolbalS.colors.inputBackground,
-    color: GolbalS.colors.textColor,
-    borderWidth: 1,
-    borderColor: GolbalS.colors.inputBorder,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  clearText: {
+    alignSelf: 'flex-end',
+    color: GolbalS.colors.errorColor,
+    fontWeight: 'bold',
     marginBottom: 12,
   },
-  sameStringEr: {
-    color: GolbalS.colors.errorColor,
-    fontSize: 14,
-    fontStyle: 'italic',
-    marginBottom: 10,
-  },
-  btn: {
-    backgroundColor: GolbalS.colors.buttonColor,
-    paddingVertical: 12,
-    paddingHorizontal: 24,
+  goalsBox: {
+    marginTop: 16,
+    flexGrow: 1,
+    backgroundColor: GolbalS.colors.inputBackground,
     borderRadius: 10,
-    marginTop: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    minHeight: 30,
-  },
-  btnContainer: {
-    flex: 4,
-  },
-  goalsBoxTextContainer: {
-    marginTop: 20,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: GolbalS.colors.goalBorderColor,
   },
 });
 
