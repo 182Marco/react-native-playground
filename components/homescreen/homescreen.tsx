9fdd2c4f63fa093814796d00f31f24dda966b13e
Milza <marcomilza@gmail.com>
2025-04-21 19:17:08 +0200

    updated homescreen main component

--- components/homescreen/homescreen.tsx
+++ components/homescreen/homescreen.tsx
@@ -1,8 +1,8 @@
 import * as N from 'react-native';
 import * as C from '../../components';
+import * as Gs from '../../styles';
 import { texts } from '../../texts';
 import { S } from './homescreen.style';
-import * as Gs from '../../styles';
 import { useHomeScreen } from './homeScreen.hook';
 
 const HomeScreen = () => {
@@ -10,6 +10,27 @@ const HomeScreen = () => {
   return (
     <N.View style={S.mainBox}>
       <N.View style={S.clearTextBox}>
+        <N.View style={S.btnBox}>
+          <C.Btn
+            text={texts.openModalBtnText}
+            onPress={() => h.setIsOpenModal(true)}
+          />
+        </N.View>
+        <C.GoalInput
+          isOpenModal={h.isOpenModal}
+          setIsOpenModal={h.setIsOpenModal}
+          goal={h.goal}
+          setGoal={h.setGoal}
+          setGoals={h.setGoals}
+          sameStringEr={h.er}
+          addSetGoals={h.addSetGoals}
+        />
+        <N.FlatList
+          data={h.goals}
+          style={S.goalsBox}
+          renderItem={g => <C.GoalItem goal={g.item} {...h.handleGoal} />}
+          alwaysBounceVertical={false}
+        />
         <N.View style={S.btnBox}>
           <C.Btn
             text={texts.clearBtnText}
@@ -20,18 +41,6 @@ const HomeScreen = () => {
             paddingVertical="0"
           />
         </N.View>
-        <C.GoalInput
-          goal={h.goal}
-          setGoal={h.setGoal}
-          sameStringEr={h.er}
-          addSetGoals={h.addSetGoals}
-        />
-        <N.FlatList
-          data={h.goals}
-          style={S.goalsBox}
-          renderItem={g => <C.GoalItem goal={g.item} {...h.handleGoal} />}
-          alwaysBounceVertical={false}
-        />
       </N.View>
     </N.View>
   );
