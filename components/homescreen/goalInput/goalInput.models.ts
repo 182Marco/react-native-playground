3fbdac596e82cb4ecab2278a911f549bb1ce6a45
Milza <marcomilza@gmail.com>
2025-04-21 19:17:20 +0200

    updated input model

--- components/homescreen/goalInput/goalInput.models.ts
+++ components/homescreen/goalInput/goalInput.models.ts
@@ -1,8 +1,13 @@
+import { IGoal } from '../homeScreen.models';
+
 interface IGoalInputProps {
   goal: string;
   sameStringEr: string;
+  isOpenModal: boolean;
+  setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
   addSetGoals: (goal: string) => void;
   setGoal: React.Dispatch<React.SetStateAction<string>>;
+  setGoals: React.Dispatch<React.SetStateAction<IGoal[]>>;
 }
 type IGoalInput = React.FC<IGoalInputProps>;
 
