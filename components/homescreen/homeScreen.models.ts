bf6ea3479c6715f6ba20edaf7a04618d5c9a4802
Milza <marcomilza@gmail.com>
2025-04-21 19:16:17 +0200

    updated homescreen models

--- components/homescreen/homeScreen.models.ts
+++ components/homescreen/homeScreen.models.ts
@@ -1,4 +1,13 @@
+interface IGoal {
+  text: string;
+  key: string;
+  isChecked: boolean;
+}
+
 type IAddSetGoals = (goal: string) => void;
 type IIsSame = (strAr: string[], str2: string) => string | undefined;
+type IIsPresent = (goals: IGoal[], enteredText: string) => string | undefined;
+type IHandleGoal = (goal: IGoal) => void;
+type IGetGoalText = (goals: IGoal[]) => string[];
 
-export { IAddSetGoals, IIsSame };
+export { IAddSetGoals, IIsSame, IGoal, IHandleGoal, IGetGoalText, IIsPresent };
