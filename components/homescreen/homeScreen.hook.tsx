80c07f803ed1d5f374fc322c28f4acfe01771ffa
Milza <marcomilza@gmail.com>
2025-04-21 19:16:07 +0200

    updated homescreen hook

--- components/homescreen/homeScreen.hook.tsx
+++ components/homescreen/homeScreen.hook.tsx
@@ -1,54 +1,54 @@
 import * as R from 'react';
-import { IAddSetGoals, IGoal } from './homeScreen.models';
-import { isSame } from './homeScreen.utils';
-import * as N from 'react-native';
+import { IAddSetGoals, IGoal, IHandleGoal } from './homeScreen.models';
+import { isPresent } from './homeScreen.utils';
+import { texts } from '../../texts';
 
 const useHomeScreen = () => {
-  const sameStringWarn = 'You entered the same string! You moron!';
+  const [isOpenModal, setIsOpenModal] = R.useState(false);
   const [goals, setGoals] = R.useState<IGoal[]>([]);
   const [goal, setGoal] = R.useState('');
-  const [sameStringEr, setSameStringEr] = R.useState('');
-  const [keyboardVisible, setKeyboardVisible] = R.useState(false);
+  const [er, setEr] = R.useState('');
 
-  R.useEffect(() => {
-    const showSub = N.Keyboard.addListener('keyboardDidShow', () => {
-      setKeyboardVisible(true);
-    });
-    const hideSub = N.Keyboard.addListener('keyboardDidHide', () => {
-      setKeyboardVisible(false);
-    });
+  class Goal implements IGoal {
+    public text: string;
+    public key: string;
+    public isChecked: boolean;
 
-    return () => {
-      showSub.remove();
-      hideSub.remove();
-    };
-  }, []);
+    constructor(g: string) {
+      this.text = g;
+      this.key = g;
+      this.isChecked = false;
+    }
+  }
 
   const addSetGoals: IAddSetGoals = () => {
     setGoal('');
     setGoals(pv => {
-      if (
-        isSame(
-          pv.map(obj => obj.text),
-          goal
-        )
-      ) {
-        setSameStringEr(sameStringWarn);
-        return pv;
-      }
-      sameStringEr === '' ? null : setSameStringEr('');
-      return [...pv, { text: goal, key: goal }];
+      if (goal === '') return setEr(texts.noStrEr()), pv;
+      if (isPresent(pv, goal)) return setEr(texts.sameStrEr()), pv;
+      er && setEr('');
+      return [...pv, new Goal(goal)];
     });
   };
 
+  const remove: IHandleGoal = goal =>
+    setGoals(pv => pv.filter(e => e.key !== goal.key));
+
+  const check: IHandleGoal = goal =>
+    setGoals(pv =>
+      pv.map(e => (e.key === goal.key ? { ...e, isChecked: !e.isChecked } : e))
+    );
+
   return {
-    goal,
-    setGoal,
-    goals,
     addSetGoals,
-    sameStringEr,
+    er,
+    goals,
+    goal,
+    handleGoal: { remove, check },
+    isOpenModal,
+    setGoal,
     setGoals,
-    keyboardVisible,
+    setIsOpenModal,
   };
 };
 
