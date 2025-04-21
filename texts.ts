07dd6b30c6747e917c86b26b47b1202def58a413
Milza <marcomilza@gmail.com>
2025-04-21 02:31:13 +0200

    updated texts

--- texts.ts
+++ texts.ts
@@ -1,7 +1,9 @@
 const texts = {
-  clearBtnText: 'Clear',
+  clearBtnText: 'Clear all',
+  goBackToListBtnText: 'Cancel',
   inputPlaceHolder: 'Add to do',
   inputGoalBtnText: 'Add',
+  openModalBtnText: 'Add new goal',
   insult: 'You damn moron!',
   sameStrEr() {
     return `You entered the same string! ${this.insult}`;
