0794844eebd4da73b21371fe227f0b31a5bc0e53
Milza <marcomilza@gmail.com>
2025-04-22 18:42:23 +0200

    homescreen updated

--- components/homescreen/homescreen.tsx
+++ components/homescreen/homescreen.tsx
@@ -9,38 +9,37 @@ const HomeScreen = () => {
   const h = useHomeScreen();
   return (
     <N.View style={S.mainBox}>
-      <N.View style={S.clearTextBox}>
-        <N.View style={S.btnBox}>
-          <C.Btn
-            text={texts.openModalBtnText}
-            onPress={() => h.setIsOpenModal(true)}
-          />
-        </N.View>
-        <C.GoalInput
-          isOpenModal={h.isOpenModal}
-          setIsOpenModal={h.setIsOpenModal}
-          goal={h.goal}
-          setGoal={h.setGoal}
-          setGoals={h.setGoals}
-          sameStringEr={h.er}
-          addSetGoals={h.addSetGoals}
+      <N.View style={S.btnBox}>
+        <C.Btn
+          text={texts.openModalBtnText}
+          onPress={() => h.setIsOpenModal(true)}
         />
-        <N.FlatList
-          data={h.goals}
-          style={S.goalsBox}
-          renderItem={g => <C.GoalItem goal={g.item} {...h.handleGoal} />}
-          alwaysBounceVertical={false}
+      </N.View>
+      <C.GoalInput
+        isOpenModal={h.isOpenModal}
+        setIsOpenModal={h.setIsOpenModal}
+        setEr={h.setEr}
+        goal={h.goal}
+        setGoal={h.setGoal}
+        setGoals={h.setGoals}
+        sameStringEr={h.er}
+        addSetGoals={h.addSetGoals}
+      />
+      <N.FlatList
+        data={h.goals}
+        style={S.goalsBox}
+        renderItem={g => <C.GoalItem goal={g.item} {...h.handleGoal} />}
+        alwaysBounceVertical={false}
+      />
+      <N.View style={S.btnBox}>
+        <C.Btn
+          text={texts.clearBtnText}
+          onPress={() => h.setGoals([])}
+          BgColor="transparent"
+          color={Gs.colors.errorColor}
+          paddingHorizontal="0"
+          paddingVertical="0"
         />
-        <N.View style={S.btnBox}>
-          <C.Btn
-            text={texts.clearBtnText}
-            BgColor="transparent"
-            color={Gs.colors.errorColor}
-            onPress={() => h.setGoals([])}
-            paddingHorizontal="0"
-            paddingVertical="0"
-          />
-        </N.View>
       </N.View>
     </N.View>
   );
