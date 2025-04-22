dfa5de35e767244c0818e79cf4e560f8e1d43f31
Milza <marcomilza@gmail.com>
2025-04-22 18:41:40 +0200

    goal input  updated

--- components/homescreen/goalInput/goalInput.tsx
+++ components/homescreen/goalInput/goalInput.tsx
@@ -1,36 +1,41 @@
 import * as N from 'react-native';
 import * as C from '../../../components';
-import { S } from './goalInput.style';
+import * as Gs from '../../../styles';
 import * as Styles from '../../../styles';
 import { texts } from '../../../texts';
 import { IGoalInput } from './goalInput.models';
+import { S } from './goalInput.style';
 
 const GoalInput: IGoalInput = p => (
-  <N.View style={S.mainBox}>
-    <N.Modal visible={p.isOpenModal} animationType="slide">
+  <N.Modal visible={p.isOpenModal} animationType="slide">
+    <N.View style={S.modalBox}>
+      <N.TextInput
+        style={S.input}
+        placeholder={texts.inputPlaceHolder}
+        placeholderTextColor={Styles.colors.textColor}
+        value={p.goal}
+        onChangeText={p.setGoal}
+      />
       <N.View style={S.inputRow}>
-        <N.TextInput
-          style={S.input}
-          placeholder={texts.inputPlaceHolder}
-          placeholderTextColor={Styles.colors.textColor}
-          value={p.goal}
-          onChangeText={p.setGoal}
-        />
         <C.Btn
           text={texts.inputGoalBtnText}
-          onPress={() => {
-            p.addSetGoals(p.goal);
-            p.setIsOpenModal(false);
-          }}
+          onPress={() => p.addSetGoals(p.goal)}
         />
         <C.Btn
           text={texts.goBackToListBtnText}
-          onPress={() => p.setIsOpenModal(false)}
+          onPress={() => {
+            p.setIsOpenModal(false);
+            p.setEr('');
+          }}
+          BgColor="transparent"
+          color={Gs.colors.errorColor}
+          paddingHorizontal="0"
+          paddingVertical="0"
         />
       </N.View>
       <N.Text style={S.sameStringEr}>{p.sameStringEr}</N.Text>
-    </N.Modal>
-  </N.View>
+    </N.View>
+  </N.Modal>
 );
 
 export { GoalInput };
