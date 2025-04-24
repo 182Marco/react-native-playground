245f7a85fb4bcba86e758b04ce879bee83efd335
Milza <marcomilza@gmail.com>
2025-04-24 11:41:02 +0200

    updated goalItem template

--- components/homescreen/goalItem/goalItem.tsx
+++ components/homescreen/goalItem/goalItem.tsx
@@ -5,17 +5,21 @@ import { s, pressedGoalLi } from './goalItem.style';
 import { IGoalItem } from './goalItem.models';
 
 const GoalItem: IGoalItem = p => (
-  <N.Pressable
-    style={p => (p.pressed ? pressedGoalLi : s.goalLiBox)}
-    {...GS.android_ripple}
-    onPress={() => p.check(p.goal)}
-  >
-    <N.View style={s.textAndCheckBox}>
-      <N.Text style={s.checkText}>{p.goal.isChecked ? '✓' : '-'}</N.Text>
-      <N.Text style={s.goalLi}>{p.goal.text}</N.Text>
-    </N.View>
-    <C.Btn text="X" onPress={() => p.remove(p.goal)} BgColor="transparent" />
-  </N.Pressable>
+  <N.View style={s.goalItemWrap}>
+    <N.Pressable
+      style={p => (p.pressed ? pressedGoalLi : s.goalLiBox)}
+      {...GS.android_ripple}
+      onPress={() => p.check(p.goal)}
+    >
+      <N.View style={s.textAndCheckBox}>
+        <N.View style={s.squareForCheck}>
+          {p.goal.isChecked && <N.Text style={s.checkSimbol}>✓</N.Text>}
+        </N.View>
+        <N.Text style={s.goalLi}>{p.goal.text}</N.Text>
+      </N.View>
+      <C.Btn text="X" onPress={() => p.remove(p.goal)} BgColor="transparent" />
+    </N.Pressable>
+  </N.View>
 );
 
 export { GoalItem };
