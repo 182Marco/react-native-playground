38c9a359ac60c6b52b48b518b4ee45ff7152c9ee
Milza <marcomilza@gmail.com>
2025-04-25 12:10:38 +0200

    updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,23 @@
-import * as R from 'react';
 import { AppRegistry, Platform } from 'react-native';
 import { HomeScreen } from '@/components';
 import appConfig from '../../app.json';
+import React, { useEffect } from 'react';
 
 const Root = () => <HomeScreen />;
 
 const { name } = appConfig.expo;
 
 if (Platform.OS === 'web') {
-  R.useEffect(() => {
-    AppRegistry.runApplication(name, {
-      initialProps: {},
-      rootTag: document.getElementById('app-root'),
-    });
+  useEffect(() => {
+    const rootTag = document.getElementById('app-root');
+    if (rootTag) {
+      AppRegistry.runApplication(name, {
+        initialProps: {},
+        rootTag,
+      });
+    } else {
+      console.error("Element with id 'app-root' not found.");
+    }
   }, []);
 } else {
   AppRegistry.registerComponent(name, () => Root);
