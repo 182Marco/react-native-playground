6b59e46a84226a9c7913fa7c473b0aa477acb404
Milza <marcomilza@gmail.com>
2025-04-25 12:02:08 +0200

    updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,18 @@
+import { AppRegistry, Platform } from 'react-native';
 import { HomeScreen } from '@/components';
+import appConfig from '../../app.json';
 
 const Root = () => <HomeScreen />;
+
+const { name } = appConfig.expo;
+
+if (Platform.OS === 'web') {
+  AppRegistry.runApplication(name, {
+    initialProps: {},
+    rootTag: document.getElementById('app-root'),
+  });
+} else {
+  AppRegistry.registerComponent(name, () => Root);
+}
 
 export default Root;
